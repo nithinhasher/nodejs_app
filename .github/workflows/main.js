1a556312b34677bb1c7e71d272d8c47378ecb823
nithinhasher <93312240+nithinhasher@users.noreply.github.com>
2023-07-14 13:22:45 +0530

    Update main.js

--- .github/workflows/main.js
+++ .github/workflows/main.js
@@ -1,12 +1,11 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!');
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
 });
 
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
